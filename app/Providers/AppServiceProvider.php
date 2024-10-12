634f2ff84eb93b2d03549d2f2b859666d571ade8
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2024-10-12 02:53:17 +0200

    https env added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-// use Illuminate\Support\Facades\URL;
-// use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL; // Uncomment this line
+use Illuminate\Support\Facades\App; // Uncomment this line
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,16 +23,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+
         // Force HTTPS in production
-        // if (App::environment('production')) {
-        //     URL::forceScheme('https');
-        // }
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
 
         // Additional production-specific configurations
         // if (App::environment('production')) {
             // Configure any production-specific services, settings, etc.
             // Example: Configure a custom cache setting
-        //     config(['cache.default' => 'redis']);
+            // config(['cache.default' => 'redis']);
         // }
     }
 }
