8a5104a5d65b7f0413ebc65404571f37960c3d80
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2025-01-16 07:57:13 +0200

    package modified

--- resources/views/Single_School/Users_acccount/Employee/Cover.blade.php
+++ resources/views/Single_School/Users_acccount/Employee/Cover.blade.php
@@ -64,8 +64,8 @@
   <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href="#" target="_blank">
-        <img src="{{ URL::to('/') }}/Single_school_account/assets/img/users_profiles_pictures/{{ auth()->guard('school_employee')->user()->image }}" alt="user_image" alt="User Image" style=" border-radius: 50%;" class="navbar-brand-img h-100" alt="main_logo">
+      <a class="navbar-brand" href="#" target="_blank">
+        <img src="{{ URL::to('/') }}/Single_school_account/assets/img/users_profiles_pictures/{{ auth()->guard('school_employee')->user()->image }}" alt="user_image"  style=" border-radius: 50%;" class="navbar-brand-img h-100">
         <span class="ms-1 font-weight-bold text-white">
           
           @if(strlen(auth()->guard('school_employee')->user()->firstname." ".auth()->guard('school_employee')->user()->middle_name." ".auth()->guard('school_employee')->user()->lastname) <= 20)
