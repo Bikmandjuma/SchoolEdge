aa0414bd8be26c2da751f61c4c56369b33f3ea0e
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2025-05-03 23:08:14 +0200

    new stuff added

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -1,9 +1,3 @@
-// const mix = require('laravel-mix');
-
-// mix.js('resources/js/app.js', 'public/js')
-//    .css('resources/css/app.css', 'public/css')
-//    .version();
-
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
@@ -14,14 +8,19 @@ mix.webpackConfig({
     module: {
         rules: [
             {
-                test: /\.m?js$/,
-                exclude: /(node_modules)/,
+                test: /\.js$/,
+                exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env']
+                        presets: ['@babel/preset-env'],
+                        sourceType: 'unambiguous'
                     }
                 }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'], // 👈 this handles CSS inside JS
             }
         ]
     }
